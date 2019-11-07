eda8746ee4a5099b0b19bd73e77eec891f6eac11
giuliarangel <giulinharangel@gmail.com>
2019-11-07 11:42:32 -0200

    hibernate 7/11

--- TP1AliGiu/src/main/java/br/edu/iff/TP1AliGiu/entidades/Usuario.java
+++ TP1AliGiu/src/main/java/br/edu/iff/TP1AliGiu/entidades/Usuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.iff.TP1AliGiu.utilidades;
+package br.edu.iff.TP1AliGiu.entidades;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
